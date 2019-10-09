0b2669ce108677fca01778be698d370d37fb8430
Martin Dezsi <dezsimartin@gmail.com>
2019-10-09 15:13:06 +0200

    absence-index added to frontend

---
+++ frontend/src/app/components/admin/absences-index/absences-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-absences-index',
+  templateUrl: './absences-index.component.html',
+  styleUrls: ['./absences-index.component.css']
+})
+export class AbsencesIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
