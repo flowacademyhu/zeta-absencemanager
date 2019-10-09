10420288a99935bda322a526e029ac466e6b9e0d
Vilmos Pásztor <pvilmos93@gmail.com>
2019-10-09 16:34:27 +0200

    admin absence show method added in service

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/AbsenceService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/AbsenceService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,5 +18,9 @@ public class AbsenceService {
 
     public List<Absence> findAllAbsence() {
         return this.absenceRepository.findAll();
+    }
+
+    public Optional<Absence> findById(Long id){
+        return absenceRepository.findById(id);
     }
 }
