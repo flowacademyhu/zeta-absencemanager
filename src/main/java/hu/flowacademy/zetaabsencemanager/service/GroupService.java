a81d16ad8bfa1881b4eefaf4bd44d73d847e151c
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-09 16:06:51 +0200

    show all group

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/GroupService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/GroupService.java
@@ -1,9 +1,12 @@
 package hu.flowacademy.zetaabsencemanager.service;
 
+import hu.flowacademy.zetaabsencemanager.model.Group;
 import hu.flowacademy.zetaabsencemanager.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,4 +14,8 @@ public class GroupService {
 
     @Autowired
     private GroupRepository groupRepository;
+
+    public List<Group> findAllGroup() {
+        return groupRepository.findAll();
+    }
 }
