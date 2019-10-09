e05106d3999c061951fec038d2c518bce7232571
Vilmos Pásztor <pvilmos93@gmail.com>
2019-10-09 15:24:17 +0200

    service method added

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/AbsenceService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/AbsenceService.java
@@ -1,9 +1,12 @@
 package hu.flowacademy.zetaabsencemanager.service;
 
+import hu.flowacademy.zetaabsencemanager.model.Absence;
 import hu.flowacademy.zetaabsencemanager.repository.AbsenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,4 +14,8 @@ public class AbsenceService {
 
     @Autowired
     private AbsenceRepository absenceRepository;
+
+    public List<Absence> findAllAbsence() {
+        return this.absenceRepository.findAll();
+    }
 }
