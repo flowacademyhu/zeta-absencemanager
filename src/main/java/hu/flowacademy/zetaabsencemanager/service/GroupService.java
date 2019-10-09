efdcdb5e1b36572308c8d447db92460e4e9fd1aa
Molsagari92 <gabi.sajti@gmail.com>
2019-10-09 16:10:10 +0200

    admin-group show

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/GroupService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/GroupService.java
@@ -1,9 +1,12 @@
 package hu.flowacademy.zetaabsencemanager.service;
 
+import hu.flowacademy.zetaabsencemanager.model.Group;
 import hu.flowacademy.zetaabsencemanager.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -11,4 +14,8 @@ public class GroupService {
 
     @Autowired
     private GroupRepository groupRepository;
+
+    public Optional<Group> findOne(Long id){
+        return groupRepository.findById(id);
+    }
 }
