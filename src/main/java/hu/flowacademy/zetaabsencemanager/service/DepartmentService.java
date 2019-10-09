1d2053041dfd401ad2583749d43c4107cd5af601
Veres Gábor <veres.gabor.attila@freemail.hu>
2019-10-09 16:01:45 +0200

    done

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/DepartmentService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/DepartmentService.java
@@ -1,9 +1,13 @@
 package hu.flowacademy.zetaabsencemanager.service;
 
+import hu.flowacademy.zetaabsencemanager.model.Department;
 import hu.flowacademy.zetaabsencemanager.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,4 +15,8 @@ public class DepartmentService {
 
     @Autowired
     private DepartmentRepository departmentRepository;
+
+    public List<Department> findAllDepartments() {
+        return departmentRepository.findAll();
+    }
 }
