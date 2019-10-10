6a7a5dce3a513d9cc369a416febfa8e2c8d65875
Vilmos Pásztor <pvilmos93@gmail.com>
2019-10-10 10:40:05 +0200

    new AdminUser and AdminAbsence service created

---
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/AdminAbsenceService.java
@@ -0,0 +1,21 @@
+package hu.flowacademy.zetaabsencemanager.service;
+
+import hu.flowacademy.zetaabsencemanager.model.Absence;
+import hu.flowacademy.zetaabsencemanager.repository.AbsenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AdminAbsenceService {
+
+    @Autowired
+    private AbsenceRepository absenceRepository;
+
+    public List<Absence> findAllAbsence() {
+        return this.absenceRepository.findAll();
+    }
+}
---
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/AdminUserService.java
@@ -0,0 +1,30 @@
+package hu.flowacademy.zetaabsencemanager.service;
+
+import hu.flowacademy.zetaabsencemanager.model.User;
+import hu.flowacademy.zetaabsencemanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class AdminUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public List<User> findAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public Optional<User> findOneUser(Long id) {
+        return userRepository.findById(id);
+    }
+}
