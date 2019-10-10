9db3d4d848e3d01d0d1f82501567ca396df1906d
Molsagari92 <gabi.sajti@gmail.com>
2019-10-10 09:57:12 +0200

    User create exception added

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/UserService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/UserService.java
@@ -3,8 +3,10 @@ package hu.flowacademy.zetaabsencemanager.service;
 import hu.flowacademy.zetaabsencemanager.model.User;
 import hu.flowacademy.zetaabsencemanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,7 +30,30 @@ public class UserService {
         return userRepository.findById(id);
     }
 
-    public User createUser(User user){
+    public User createUser(User user) {
+        if (user == null
+                || user.getFirstName() == null
+                || user.getFirstName().equals("")
+                || user.getLastName() == null
+                || user.getLastName().equals("")
+                || user.getDateOfBirth() == null
+                || user.getEmail() == null
+                || user.getEmail().equals("")
+                || user.getDateOfEntry() == null
+                || user.getDateOfEndTrial() == null
+                || user.getIsOnTrial() == null
+                || user.getDepartments() == null
+                || user.getDepartments().size() == 0
+                || user.getGroups() == null
+                || user.getGroups().size() == 0
+                || user.getPosition() == null
+                || user.getPosition().equals("")
+                || user.getRole() == null
+                || user.getNumberOfChildren() == null
+                || user.getOtherAbsenceEnt() == null
+        ) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The submitted arguments are invalid.");
+        }
         userRepository.save(user);
         return user;
     }
