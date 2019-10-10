d36067a8ab8f851759be7c8b189ef5972b893fb8
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-10 10:39:23 +0200

    admin group update

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/GroupService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/GroupService.java
@@ -3,8 +3,10 @@ package hu.flowacademy.zetaabsencemanager.service;
 import hu.flowacademy.zetaabsencemanager.model.Group;
 import hu.flowacademy.zetaabsencemanager.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,8 +21,29 @@ public class GroupService {
     public List<Group> findAllGroup() {
         return groupRepository.findAll();
     }
-  
-    public Optional<Group> findOne(Long id){
+
+    public Optional<Group> findOneGroup(Long id) {
         return groupRepository.findById(id);
+    }
+
+    public Group updateGroup(Long id, Group group) {
+        Group modifyGroup = findOneGroup(id).orElse(null);
+        if (modifyGroup == null ||
+                group.getDepartment() == null ||
+                group.getName() == null ||
+                group.getName().equals("") ||
+                group.getEmployees() == null ||
+                group.getEmployees().size() == 0 ||
+                group.getLeaders() == null ||
+                group.getLeaders().size() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The submitted arguments are invalid.");
+        } else {
+            modifyGroup.setName(group.getName());
+            modifyGroup.setDepartment(group.getDepartment());
+            modifyGroup.setEmployees(group.getEmployees());
+            modifyGroup.setLeaders(group.getLeaders());
+            groupRepository.save(modifyGroup);
+            return modifyGroup;
+        }
     }
 }
