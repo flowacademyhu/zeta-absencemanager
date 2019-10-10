e317e03f721c5adb370b3424a19a6c9263d2c85f
Veres Gábor <veres.gabor.attila@freemail.hu>
2019-10-10 14:35:15 +0200

    admin-absence-service

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/AdminAbsenceService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/AdminAbsenceService.java
@@ -1,12 +1,27 @@
 package hu.flowacademy.zetaabsencemanager.service;
 
 import hu.flowacademy.zetaabsencemanager.model.Absence;
+import hu.flowacademy.zetaabsencemanager.model.Status;
+import hu.flowacademy.zetaabsencemanager.model.Type;
+import hu.flowacademy.zetaabsencemanager.model.User;
 import hu.flowacademy.zetaabsencemanager.repository.AbsenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,5 +32,34 @@ public class AdminAbsenceService {
 
     public List<Absence> findAllAbsence() {
         return this.absenceRepository.findAll();
+    }
+
+    public Absence findOne(@NotNull Long id){
+        return absenceRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "The submitted arguments are invalid."));
+    }
+
+    public Absence create(Absence absence) {
+        if (absence.getType() == null || absence.getBegin() == null || absence.getEnd() == null || absence.getReporter() == null ||
+                absence.getAssignee() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The submitted arguments are invalid.");
+        }
+        return absenceRepository.save(absence);
+    }
+
+    public Absence update (@NotNull Long id, @NotNull Absence absence) {
+        Absence modifyAbsence = absenceRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "The submitted arguments are invalid."));
+        modifyAbsence.setAssignee(absence.getAssignee());
+        modifyAbsence.setBegin(absence.getBegin());
+        modifyAbsence.setEnd(absence.getEnd());
+        modifyAbsence.setReporter(absence.getReporter());
+        modifyAbsence.setStatus(absence.getStatus());
+        modifyAbsence.setType(absence.getType());
+        absenceRepository.save(modifyAbsence);
+        return modifyAbsence;
+    }
+
+    public void delete (@NotNull Long id) {
+        absenceRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "The submitted arguments are invalid."));
+        absenceRepository.deleteById(id);
     }
 }
