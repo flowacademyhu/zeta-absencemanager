f12210fe16862d42fb42b4a0d04eff9f46392f8b
Veres Gábor <veres.gabor.attila@freemail.hu>
2019-10-14 12:28:21 +0200

    repair exception throwing in the CustomUserDetailsService

--- src/main/java/hu/flowacademy/zetaabsencemanager/auth/CustomUserDetailsService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/auth/CustomUserDetailsService.java
@@ -26,9 +26,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         final hu.flowacademy.zetaabsencemanager.model.User user = userRepository.findByEmail(s).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "The submitted arguments are invalid."));
-        if (user == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "The submitted arguments are invalid.");
-        }
+
         return new User(s, user.getPassword(), List.of(new SimpleGrantedAuthority(user.getRole().toString())));
     }
 
