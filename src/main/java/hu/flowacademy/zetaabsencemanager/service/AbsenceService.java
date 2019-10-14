3af50a3162789592e71690f41fed5ef005c98b47
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-14 12:51:58 +0200

    fix auth if null

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/AbsenceService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/AbsenceService.java
@@ -5,6 +5,7 @@ import hu.flowacademy.zetaabsencemanager.model.Status;
 import hu.flowacademy.zetaabsencemanager.repository.AbsenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,10 +25,11 @@ public class AbsenceService {
     private UserService userService;
 
     public Long getCurrentUser() {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        if (email == null) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "user not found");
         }
+        String email = auth.getName();
         return userService.findByEmail(email).getId();
     }
 
