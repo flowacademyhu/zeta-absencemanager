58b29e414bfc1994d54872b90ae340defd3d64b8
Martin Dezsi <dezsimartin@gmail.com>
2019-10-15 15:02:55 +0200

    modified connectors

--- frontend/src/app/models/connectors/AuthConnector.ts
+++ frontend/src/app/models/connectors/AuthConnector.ts
@@ -12,49 +12,48 @@ export class AuthConnector<T> extends AbstractApiConnector<T>{
     private _userData$: BehaviorSubject<User> = new BehaviorSubject(undefined);
     private _isLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject(this.hasToken());
 
-    constructor(protected _http: HttpClient, protected connector : Connectors, private router : Router){
+    constructor(protected _http: HttpClient, private router : Router){
         super(_http, Connectors.AUTH);
     }
 
-    login(username: string, password: string) {
+    public login(username: string, password: string) : Promise<boolean>{
+      const body = new HttpParams()
+        .set('username', username)
+        .set('password', password)
+        .set('grant_type', 'password');
 
-        const body = new HttpParams()
-            .set('username', username)
-            .set('password', password)
-            .set('grant_type', 'password');
-
-        return new Promise<any>((resolve, reject) => {  
-            this._http.post(this.baseURL + 'oauth/token', body, this.httpOptions).subscribe((data : any) => {
-              localStorage.setItem('token', data.access_token);
-              this._isLoggedIn$.next(true);
-              this.router.navigate(["admin/absence-index"]);
-              resolve();
-            }, (error: HttpErrorResponse) => {
-              //Invalid credentials
-              if (error.status === 401 || error.status === 400) {
-                reject(LoginRejectionReason.BAD_CREDENTIALS);
-              } else if (error.status === 0 || error.status == 503) {
-                reject(LoginRejectionReason.SERVICE_UNAVAILABLE);
-              } else {
-                reject(LoginRejectionReason.UNKNOWN);
-              }
-            });
-          });
+      return new Promise<any>((resolve, reject) => {  
+        this._http.post(this.baseURL + 'oauth/token', body, this.httpOptions).subscribe((data : any) => {
+          localStorage.setItem('token', data.access_token);
+          this._isLoggedIn$.next(true);
+          this.router.navigate(["admin/absence-index"]);
+          resolve();
+        }, (error: HttpErrorResponse) => {
+          //Invalid credentials
+          if (error.status === 401 || error.status === 400) {
+            reject(LoginRejectionReason.BAD_CREDENTIALS);
+          } else if (error.status === 0 || error.status == 503) {
+            reject(LoginRejectionReason.SERVICE_UNAVAILABLE);
+          } else {
+            reject(LoginRejectionReason.UNKNOWN);
+          }
+        });
+      });
     }
 
     public logout(): Promise<boolean> {
-        return new Promise<boolean>(resolve => {
+      return new Promise<boolean>(resolve => {
           localStorage.removeItem('token');
           this._isLoggedIn$.next(false);
           resolve(true);
         });
       }
 
-    public hasToken() : boolean {
-        return !!localStorage.getItem('token');
+    public hasToken(): boolean {
+      return !!localStorage.getItem('token');
     }
     
-    public isLoggedIn() : Observable<boolean> {
+    public isLoggedIn(): Observable<boolean> {
       return this._isLoggedIn$;
     }
 
