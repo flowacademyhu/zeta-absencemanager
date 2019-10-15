c2e421cd236b9661c2fe98fe3327d45478cdf6d8
Richard Csiki <richard.csiki@gmail.com>
2019-10-15 10:47:02 +0200

    changes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminUserShowComponent } from './components/admin/user-index/admin-user-index/admin-user-show.component';
-import { AbsencesIndexComponent } from './components/admin/absences-index/absences-index.component';
+import { AdminAbsencesIndexComponent } from './components/admin/absences-index/admin-absences-index.component';
 import { LoginComponent } from './components/login/login.component';
-import { AbsenceIndexComponent } from './components/employee/emp-absence-index/absence-index.component';
+import { UserAbsenceIndexComponent } from './components/employee/emp-absence-index/user-absence-index.component';
 import { AdminUserEditDestroyShowComponent } from './components/admin/user-edit-destroy-show/admin-user-edit-destroy-show/admin-user-edit-destroy-show.component';
 
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "login", component: LoginComponent },
-  { path: "admin/absence-index", component: AbsencesIndexComponent },
+  { path: "admin/absence-index", component: AdminAbsencesIndexComponent },
   { path: 'admin/user-index', component: AdminUserShowComponent },
-  { path: "user/absence-index", component: AbsenceIndexComponent },
+  { path: "user/absence-index", component: UserAbsenceIndexComponent },
   { path: "admin/user-esd", component: AdminUserEditDestroyShowComponent}
 ]
 
