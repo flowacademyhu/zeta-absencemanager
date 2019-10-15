7e6d00eec23be82fa259331c75f796b3114e86df
Laszlo <gyuris.laszlo84@gmail.com>
2019-10-15 12:03:52 +0200

    employee-show-template

--- frontend/src/app/components/employee/employee-show/employee-show.component.ts
+++ frontend/src/app/components/employee/employee-show/employee-show.component.ts
@@ -2,19 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource, MatFormFieldControl } from '@angular/material';
 import { User } from 'src/app/models/User.model';
 
- export interface UserElement {
-  name: string;
-  dob: string;
-  position: string;
-  supervisor: string;
-  doe: string;
-  email: string;
-} 
-
-const Data: UserElement = 
-  {name: 'employee', dob: '1990-10-11', position: 'developer', supervisor: 'ceo', doe: '2019-10-10', email: 'emp@samplemail.com'},
- 
-
 
 @Component({
   selector: 'app-employee-show',
@@ -22,6 +9,20 @@ const Data: UserElement =
   styleUrls: ['./employee-show.component.css']
 })
 export class EmployeeShowComponent implements OnInit {
+  public name = "employee";
+  public dob = '1990-10-11';
+  public doe = '2000-10-10';
+  public trialexp = '2001-01-01';
+  public position = 'melós';
+  public supervisor = "Git Áron"
+  public numberOfChildren = 3;
+  public other = '';
+
+  public total = 20;
+  public used = 10;
+  public aviable = this.total-this.used; 
+  public totalrate = this.used/this.total
+
 
   constructor() { }
 
