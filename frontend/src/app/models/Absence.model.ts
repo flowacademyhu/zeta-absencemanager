23b7dfc705a166bdd9ebacca59a6a281fc13edec
Martin Dezsi <dezsimartin@gmail.com>
2019-10-15 22:45:30 +0200

    user,group, absence model completely updated, modified. optional variables added, abstract data entity created

--- frontend/src/app/models/Absence.model.ts
+++ frontend/src/app/models/Absence.model.ts
@@ -1,19 +1,41 @@
 import { User } from './User.model';
+import { DataEntity } from './DataEntity';
 
-export class Absence {
-    constructor(
-        public id: number,
-        public createdAt: Date,
-        public summary: number,
-        public begin: Date,
-        public end: Date,
-        public reporter: User,
-        public assignee: User,
-        public status: string,
-        public type: string
-        
+//TODO enum names imported from backend, we should consider the names as well
+export enum AbsenceType {
+    ABSENCE,
+    NON_WORKING,
+    CHILD_SICK_PAY,
+    UNPAID_HOLIDAY
+}
 
-    ){}
+export enum Status {
+    OPEN,
+    UNDER_REVIEW,
+    APPROVED,
+    ADMINISTRATED,
+    DONE,
+    REJECT
+}
+
+export class Absence extends DataEntity{
+        public type: AbsenceType;
+        public summary: string;
+        public begin: Date;
+        public end: Date;
+        public reporter: User;
+        public assignee: User;
+        public status: Status;
+
+    constructor(type: AbsenceType, summary: string, begin: Date, end: Date, reporter?: User, assignee?: User) {
+        super();
+        this.type = type;
+        this.summary = summary;
+        this.begin = begin;
+        this.end = end;
+        this.reporter = reporter;
+        this.assignee = assignee;
+    }
 }
 
 
