f2bb3c9c32623e3a6b39c6eb310ffdfd8e024aa1
Laszlo <gyuris.laszlo84@gmail.com>
2019-10-15 10:09:26 +0200

    mod2

--- frontend/src/app/components/employee/employee-show/employee-show.component.ts
+++ frontend/src/app/components/employee/employee-show/employee-show.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource, MatFormFieldControl } from '@angular/material';
+import { User } from 'src/app/models/User.model';
 
-export interface UserElement {
+ export interface UserElement {
   name: string;
   dob: string;
   position: string;
   supervisor: string;
   doe: string;
   email: string;
-}
+} 
 
-const Data: UserElement[] = [
+const Data: UserElement = 
   {name: 'employee', dob: '1990-10-11', position: 'developer', supervisor: 'ceo', doe: '2019-10-10', email: 'emp@samplemail.com'},
-  {name: 'employeenr2', dob: '1995-10-11', position: 'staff', supervisor: 'ceo', doe: '2018-10-10', email: 'employee@samplemail.com'}
-]
+ 
+
 
 @Component({
   selector: 'app-employee-show',
@@ -21,9 +22,6 @@ const Data: UserElement[] = [
   styleUrls: ['./employee-show.component.css']
 })
 export class EmployeeShowComponent implements OnInit {
-
-  displayedColumns: string[] = ['name', 'dob', 'position', 'supervisor', 'doe', 'email'];
-  dataSource = new MatTableDataSource(Data); // --> filter
 
   constructor() { }
 
