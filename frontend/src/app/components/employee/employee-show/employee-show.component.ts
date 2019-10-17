613274087634b45f8e0abdfc4c6f3fcffc40b63a
Laszlo <gyuris.laszlo84@gmail.com>
2019-10-17 16:36:06 +0200

    connect to backend

--- frontend/src/app/components/employee/employee-show/employee-show.component.ts
+++ frontend/src/app/components/employee/employee-show/employee-show.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -13,8 +14,8 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./employee-show.component.css']
 })
 export class EmployeeShowComponent implements OnInit {
-  private _unsubscribe$: Subject<boolean> = new Subject();
-  public user: User = new User();
+    private _unsubscribe$: Subject<boolean> = new Subject();
+    public user: User;
 
   
 public name = "employee";
@@ -31,30 +32,35 @@ public name = "employee";
   public aviable = this.total-this.used; 
   public totalrate = this.used/this.total
   
-  constructor(private formbuild: FormBuilder, private route: ActivatedRoute) { }
+      constructor(private userService: UserService,  private formbuild: FormBuilder, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.route.data
-    .pipe(takeUntil(this._unsubscribe$))
-    .subscribe((data: any) => {
-      this.user = data.user;
-      console.log(this.user);
-    })
-  }
+    ngOnInit() {
+      this.route.data
+      .pipe(takeUntil(this._unsubscribe$))
+      .subscribe((data: any) => {
+        this.user = data.user;
+        console.log(this.user);
+        this.user;
+      })
+  this.showuser();
+    }
+  
+    ngOnDestroy(): void {
+      this._unsubscribe$.next();
+      this._unsubscribe$.complete();
+      
+    }
+    
+  showuser(){
+    this.userService.getUser(1).subscribe((user) => {
+    this.user = user;
+       })
 
-  ngOnDestroy(): void {
-    this._unsubscribe$.next();
-    this._unsubscribe$.complete();
-    
-  }
-
-  update() {
-    this.user = this.user
-   
-  }
-
-  
+       
+ }
 }
+
+  
 
 
 
