06340b5a03c8b3cff306a86fb22f3a5153392af1
Martin Dezsi <dezsimartin@gmail.com>
2019-10-17 11:46:29 +0200

    modified absence component with http method

--- frontend/src/app/components/admin/absences-index/absences-index.component.ts
+++ frontend/src/app/components/admin/absences-index/absences-index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Absence } from 'src/app/models/Absence.model';
+import { ApiCommunicationService } from 'src/app/services/ApiCommunication.service';
 
 const MOCK_DATA: any[] = [
   {id: 1, name: 'Hydrogen', begin: 1.0079, end: 'H', days: '3', type: 'absence', status: 'open', created_at: 1990-10-10, responsible: 'mockRes', reporter: 'Reporter', on_trial: true},
@@ -12,11 +14,12 @@ const MOCK_DATA: any[] = [
 })
 export class AbsencesIndexComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'begin', 'end', 'days', 'type', 'status', 'created_at', 'responsible', 'reporter', 'on_trial'];
-  dataSource = MOCK_DATA;
+  dataSource: Absence[];
 
-  constructor() { }
+  constructor(public api: ApiCommunicationService) { }
 
   ngOnInit() {
+    this.api.adminAbsence().getAbsences().subscribe(absences => {this.dataSource = absences; console.log(this.dataSource);});
   }
 
 }
