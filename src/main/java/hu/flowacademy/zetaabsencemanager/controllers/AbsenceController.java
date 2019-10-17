5abec10e40d8dc373d5dc19978950d43013c3925
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-17 11:30:06 +0200

    absence controller delete comments

--- src/main/java/hu/flowacademy/zetaabsencemanager/controllers/AbsenceController.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/controllers/AbsenceController.java
@@ -17,26 +17,21 @@ public class AbsenceController {
     @GetMapping("/{id}")
     public Absence getOne(@PathVariable("id") Long id) {
         return absenceService.findOne(id);
-       /* Absence absence = Absence.builder().build();
-        return absence;*/
     }
 
     @GetMapping("")
     public List<Absence> getAll() {
         return absenceService.findAll();
-        //return new ArrayList<>();
     }
 
     @PostMapping("")
     public Absence create(@RequestBody Absence absence) {
         return absenceService.create(absence);
-        //return absence;
     }
 
     @PutMapping("/{id}")
     public Absence update(@PathVariable("id") Long id, @RequestBody Absence absence) {
         return absenceService.update(id, absence);
-        //return absence;
     }
 
 }
