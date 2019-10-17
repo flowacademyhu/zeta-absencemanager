03d120ccd9fc7df63a9172c6e3c38a80109c69e9
Laszlo <gyuris.laszlo84@gmail.com>
2019-10-17 17:11:53 +0200

    corrections

--- frontend/src/app/components/employee/employee-show/employee-show.component.ts
+++ frontend/src/app/components/employee/employee-show/employee-show.component.ts
@@ -17,22 +17,7 @@ export class EmployeeShowComponent implements OnInit {
     private _unsubscribe$: Subject<boolean> = new Subject();
     public user: User;
 
-  
-public name = "employee";
-  public dob = '1990-10-11';
-  public doe = '2000-10-10';
-  public trialexp = '2001-01-01';
-  public position = 'melós';
-  public supervisor = "Git Áron"
-  public numberOfChildren = 3;
-  public other = '';
-
-  public total = 20;
-  public used = 10;
-  public aviable = this.total-this.used; 
-  public totalrate = this.used/this.total
-  
-      constructor(private userService: UserService,  private formbuild: FormBuilder, private route: ActivatedRoute) { }
+constructor(private userService: UserService,  private formbuild: FormBuilder, private route: ActivatedRoute) { }
 
     ngOnInit() {
       this.route.data
