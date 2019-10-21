84d9687f03cf935b345adacdda0527ad9f94d009
Molsagari92 <gabi.sajti@gmail.com>
2019-10-21 15:48:38 +0200

    updated swagger security

--- src/main/java/hu/flowacademy/zetaabsencemanager/config/SwaggerConfig.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/config/SwaggerConfig.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -19,8 +24,54 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()));
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId("fooClientIdPassword")
+                .clientSecret("secret")
+                .scopeSeparator(" ")
+                .useBasicAuthenticationWithAccessCodeGrant(true)
                 .build();
     }
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+                .tokenEndpoint(new TokenEndpoint( "http://localhost:8080"+"/token", "oauthtoken"))
+                .tokenRequestEndpoint(
+                        new TokenRequestEndpoint("http://localhost:8080"+"/authorize", "fooClientIdPassword", "secret"))
+                .build();
+
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+                .grantTypes(Arrays.asList(grantType))
+                .scopes(Arrays.asList(scopes()))
+                .build();
+        return oauth;
+    }
+
+
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScopeBuilder().scope("read").description("for read operations").build(),
+                new AuthorizationScopeBuilder().scope("write").description("for write operations").build(),
+                new AuthorizationScopeBuilder().scope("foo").description("Access foo API").build()};
+        return scopes;
+    }
+
+
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(
+                        Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+                .forPaths(PathSelectors.regex("/foos.*"))
+                .build();
+    }
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
