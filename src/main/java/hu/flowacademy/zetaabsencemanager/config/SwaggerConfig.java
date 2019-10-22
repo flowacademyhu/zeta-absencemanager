ebf8ff03b2244fb4dd345980a9eacecb836a55e4
Molsagari92 <gabi.sajti@gmail.com>
2019-10-22 17:07:59 +0200

    update

--- src/main/java/hu/flowacademy/zetaabsencemanager/config/SwaggerConfig.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/config/SwaggerConfig.java
@@ -5,15 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.*;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
 import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableSwagger2
@@ -22,53 +27,27 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("hu.flowacademy.zetaabsencemanager.controllers"))
                 .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(Arrays.asList(securityScheme()))
-                .securityContexts(Arrays.asList(securityContext()));
-    }
-
-    @Bean
-    public SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder()
-                .clientId("fooClientIdPassword")
-                .clientSecret("secret")
-                .scopeSeparator(" ")
-                .useBasicAuthenticationWithAccessCodeGrant(true)
-                .build();
-    }
-
-    private SecurityScheme securityScheme() {
-        GrantType grantType = new AuthorizationCodeGrantBuilder()
-                .tokenEndpoint(new TokenEndpoint( "http://localhost:8080"+"/token", "oauthtoken"))
-                .tokenRequestEndpoint(
-                        new TokenRequestEndpoint("http://localhost:8080"+"/authorize", "fooClientIdPassword", "secret"))
-                .build();
-
-        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
-                .grantTypes(Arrays.asList(grantType))
-                .scopes(Arrays.asList(scopes()))
-                .build();
-        return oauth;
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(Arrays.asList(new ApiKey("Bearer %token", "Authorization", "Header")));
     }
 
 
     private AuthorizationScope[] scopes() {
-        AuthorizationScope[] scopes = {
+        return new AuthorizationScope[]{
                 new AuthorizationScopeBuilder().scope("read").description("for read operations").build(),
-                new AuthorizationScopeBuilder().scope("write").description("for write operations").build(),
-                new AuthorizationScopeBuilder().scope("foo").description("Access foo API").build()};
-        return scopes;
+                new AuthorizationScopeBuilder().scope("write").description("for write operations").build()};
     }
 
 
 
     private SecurityContext securityContext() {
         return SecurityContext.builder()
-                .securityReferences(
-                        Arrays.asList(new SecurityReference("spring_oauth", scopes())))
-                .forPaths(PathSelectors.regex("/foos.*"))
+//                .securityReferences(
+//                        List.of(new SecurityReference("spring_oauth", scopes())))
+//                .forPaths(PathSelectors.regex("/*"))
                 .build();
     }
 
