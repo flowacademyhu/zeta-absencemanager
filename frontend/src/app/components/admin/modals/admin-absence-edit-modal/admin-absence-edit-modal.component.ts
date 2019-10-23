372a301202d6b3886df53873cd5749a69fbc7b1f
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-23 22:46:07 +0200

    add patchvalue, ondestroy

--- frontend/src/app/components/admin/modals/admin-absence-edit-modal/admin-absence-edit-modal.component.ts
+++ frontend/src/app/components/admin/modals/admin-absence-edit-modal/admin-absence-edit-modal.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { Component, OnInit, Inject, OnDestroy } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Absence, AbsenceType } from "src/app/models/Absence.model";
 import { Subject } from "rxjs";
-import { ActivatedRoute } from "@angular/router";
 import { ApiCommunicationService } from "src/app/services/api-communication.service";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 import { takeUntil } from "rxjs/operators";
@@ -13,8 +12,8 @@ import { User } from "src/app/models/User.model";
   templateUrl: "./admin-absence-edit-modal.component.html",
   styleUrls: ["./admin-absence-edit-modal.component.css"]
 })
-export class AdminAbsenceEditModalComponent implements OnInit {
-  private createAbsenceForm: FormGroup = new FormGroup({});
+export class AdminAbsenceEditModalComponent implements OnInit, OnDestroy {
+  private createAbsenceForm: FormGroup;
   private types;
   private error: string;
   private absence: Absence;
@@ -25,13 +24,20 @@ export class AdminAbsenceEditModalComponent implements OnInit {
   private users: User[];
 
   constructor(
-    private route: ActivatedRoute,
     private api: ApiCommunicationService,
     private dialogRef: MatDialogRef<AdminAbsenceEditModalComponent>,
     @Inject(MAT_DIALOG_DATA) private data: any
   ) {}
 
   ngOnInit() {
+    this.createAbsenceForm = new FormGroup({
+      type: new FormControl("", Validators.required),
+      summary: new FormControl(""),
+      begin: new FormControl("", Validators.required),
+      end: new FormControl("", Validators.required),
+      reporter: new FormControl("", Validators.required),
+      assignee: new FormControl("", Validators.required)
+    });
     this.api
       .user()
       .getLeaders()
@@ -52,19 +58,22 @@ export class AdminAbsenceEditModalComponent implements OnInit {
           this.absence = data;
           this.absence.begin = new Date(
             data.begin[0],
-            data.begin[1],
+            data.begin[1] + 1,
             data.begin[2]
           );
-          this.absence.end = new Date(data.end[0], data.end[1], data.end[2]);
+          this.absence.end = new Date(
+            data.end[0],
+            data.end[1] + 1,
+            data.end[2]
+          );
           this.types = Absence.enumSelector(AbsenceType);
-          console.log(this.absence.reporter);
-          this.createAbsenceForm = new FormGroup({
-            type: new FormControl(this.absence.type),
-            summary: new FormControl(this.absence.summary),
-            start: new FormControl(this.absence.begin),
-            end: new FormControl(this.absence.end),
-            reporter: new FormControl(this.absence.reporter), //TODO doesn't appear in the input field
-            assignee: new FormControl(this.absence.assignee)
+          this.createAbsenceForm.patchValue({
+            type: this.absence.type,
+            summary: this.absence.summary,
+            begin: this.absence.begin,
+            end: this.absence.end,
+            reporter: this.absence.reporter,
+            assignee: this.absence.assignee
           });
         },
         err => {
@@ -73,17 +82,12 @@ export class AdminAbsenceEditModalComponent implements OnInit {
       );
   }
 
-  public OnSubmit(createAbsenceFormValue): void {
+  public onSubmit(): void {
     if (this.update) {
       this.update = !this.update;
       this.message = "Edit";
       if (this.createAbsenceForm.valid) {
-        this.absence.type = createAbsenceFormValue.type;
-        this.absence.summary = createAbsenceFormValue.summary;
-        this.absence.begin = createAbsenceFormValue.start;
-        this.absence.end = createAbsenceFormValue.end;
-        this.absence.reporter = createAbsenceFormValue.reporter;
-        this.absence.assignee = createAbsenceFormValue.assignee;
+        this.absence = this.createAbsenceForm.getRawValue();
         this.api
           .adminAbsence()
           .updateAbsence(this.absence.id, this.absence)
