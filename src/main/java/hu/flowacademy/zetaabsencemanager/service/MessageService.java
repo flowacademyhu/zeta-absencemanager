4975bc36e1185c7801112601a1c5b9d3113e8200
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-24 14:22:52 +0200

    add google account

--- src/main/java/hu/flowacademy/zetaabsencemanager/service/MessageService.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/service/MessageService.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
 import java.io.IOException;
 
+@Service
 public class MessageService {
     @Autowired
     private JavaMailSender javaMailSender;
