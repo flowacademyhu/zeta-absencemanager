cf161977a8397e5f71773d2b8a58fd87a7f5189a
Vilmos Pásztor <pvilmos93@gmail.com>
2019-10-24 16:29:54 +0200

    removed unnecessary imports and event enum

--- src/main/java/hu/flowacademy/zetaabsencemanager/ZetaAbsencemanagerApplication.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/ZetaAbsencemanagerApplication.java
@@ -1,18 +1,10 @@
 package hu.flowacademy.zetaabsencemanager;
 
-import hu.flowacademy.zetaabsencemanager.repository.AbsenceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationEventPublisher;
 
 @SpringBootApplication
 public class ZetaAbsencemanagerApplication {
-
-  @Autowired
-  private ApplicationEventPublisher publisher;
-  @Autowired
-  private AbsenceRepository store;
 
   public static void main(String[] args) {
     SpringApplication.run(ZetaAbsencemanagerApplication.class, args);
