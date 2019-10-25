4258d73653808054b978e70228d922fa8ef1aa81
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-10-25 11:12:19 +0200

    refactor code

--- frontend/src/app/components/admin/modals/admin-absence-edit-modal/admin-absence-edit-modal.component.ts
+++ frontend/src/app/components/admin/modals/admin-absence-edit-modal/admin-absence-edit-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject, OnDestroy } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Absence, AbsenceType } from "src/app/models/Absence.model";
-import { Subject } from "rxjs";
+import { Subject, Observable, forkJoin } from "rxjs";
 import { ApiCommunicationService } from "src/app/services/api-communication.service";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 import { takeUntil } from "rxjs/operators";
@@ -18,17 +18,18 @@ export class AdminAbsenceEditModalComponent implements OnInit, OnDestroy {
   private types;
   private error: string;
   private absence: Absence;
-  private message = "Edit";
   private _unsubscribe$ = new Subject<void>();
   private leaders: User[];
   private users: User[];
-  private isTypeEdit = { value: false };
-  private isSummaryEdit = { value: false };
-  private isBeginEdit = { value: false };
-  private isEndEdit = { value: false };
-  private isDurationEdit = { value: false };
-  private isReporterEdit = { value: false };
-  private isAssigneeEdit = { value: false };
+  private formEditState = {
+    isTypeEdited: false,
+    isSummaryEdited: false,
+    isBeginEdited: false,
+    isEndEdited: false,
+    isDurationEdited: false,
+    isReporterEdited: false,
+    isAssigneeEdited: false
+  };
   private duration = 0;
   private dates = [false, false];
 
@@ -48,99 +49,51 @@ export class AdminAbsenceEditModalComponent implements OnInit, OnDestroy {
       reporter: new FormControl("", Validators.required),
       assignee: new FormControl("", Validators.required)
     });
-    this.api
-      .user()
-      .getLeaders()
-      .subscribe(data => {
-        this.leaders = data;
-        this.api
-          .user()
-          .getUsers()
-          .subscribe(data => {
-            this.users = data;
-            this.api
-              .adminAbsence()
-              .getAbsence(this.data.absence.id)
-              .subscribe(
-                data => {
-                  this.absence = data;
-                  this.absence.begin = new Date(
-                    data.begin[0],
-                    data.begin[1] - 1,
-                    data.begin[2]
-                  );
-                  this.absence.end = new Date(
-                    data.end[0],
-                    data.end[1] - 1,
-                    data.end[2]
-                  );
-                  this.types = Absence.enumSelector(AbsenceType);
-                  this.createAbsenceForm.patchValue({
-                    type: this.absence.type,
-                    summary: this.absence.summary,
-                    begin: this.absence.begin,
-                    end: this.absence.end,
-                    duration: this.absence.duration,
-                    reporter: this.users.find(
-                      e => e.id === this.absence.reporter.id
-                    ),
-                    assignee: this.leaders.find(
-                      e => e.id === this.absence.assignee.id
-                    )
-                  });
-                },
-                err => {
-                  this.error = err.error.message;
-                }
-              );
-          });
-      });
+
+    this.requestDatas().subscribe(
+      responseList => {
+        this.leaders = responseList[0];
+        this.users = responseList[1];
+        this.absence = responseList[2];
+        this.absence.begin = new Date(
+          responseList[2].begin[0],
+          responseList[2].begin[1] - 1,
+          responseList[2].begin[2]
+        );
+        this.absence.end = new Date(
+          responseList[2].end[0],
+          responseList[2].end[1] - 1,
+          responseList[2].end[2]
+        );
+        this.types = Absence.enumSelector(AbsenceType);
+        this.createAbsenceForm.patchValue({
+          type: this.absence.type,
+          summary: this.absence.summary,
+          begin: this.absence.begin,
+          end: this.absence.end,
+          duration: this.absence.duration,
+          reporter: this.users.find(e => e.id === this.absence.reporter.id),
+          assignee: this.leaders.find(e => e.id === this.absence.assignee.id)
+        });
+      },
+      err => {
+        this.error = err.error.message;
+      }
+    );
+  }
+
+  public requestDatas(): Observable<any[]> {
+    let leaders = this.api.user().getLeaders();
+    let users = this.api.user().getUsers();
+    let absence = this.api.adminAbsence().getAbsence(this.data.absence.id);
+    return forkJoin([leaders, users, absence]);
   }
 
   public onSubmit(): void {
-    if (
-      this.isTypeEdit.value ||
-      this.isSummaryEdit.value ||
-      this.isBeginEdit.value ||
-      this.isEndEdit.value ||
-      this.isReporterEdit.value ||
-      this.isAssigneeEdit.value ||
-      this.isDurationEdit.value
-    ) {
-      this.isTypeEdit.value = false;
-      this.isSummaryEdit.value = false;
-      this.isBeginEdit.value = false;
-      this.isEndEdit.value = false;
-      this.isReporterEdit.value = false;
-      this.isAssigneeEdit.value = false;
-      this.isDurationEdit.value = false;
-
-      this.message = "Edit";
+    if (this.isFormEdited()) {
+      this.setFormEdited(false);
       if (this.createAbsenceForm.valid) {
-        this.absence.type = this.createAbsenceForm.controls["type"].value;
-        this.absence.summary = this.createAbsenceForm.controls["summary"].value;
-        this.absence.begin = Absence.convertDate(
-          this.createAbsenceForm.controls["begin"].value
-        );
-        this.absence.end = Absence.convertDate(
-          this.createAbsenceForm.controls["end"].value
-        );
-        this.absence.reporter = this.createAbsenceForm.controls[
-          "reporter"
-        ].value;
-        this.absence.assignee = this.createAbsenceForm.controls[
-          "assignee"
-        ].value;
-        this.absence.duration = this.createAbsenceForm.controls[
-          "duration"
-        ].value;
-        /*         if (this.absence.duration === 0 || this.absence.duration == null) {
-          var begin = moment(this.absence.begin);
-          var end = moment(this.absence.end);
-          var duration =
-            Math.floor(moment.duration(end.diff(begin)).asDays()) + 1;
-          this.absence.duration = duration;
-        } */
+        this.getFormData();
         this.api
           .adminAbsence()
           .updateAbsence(this.absence.id, this.absence)
@@ -153,20 +106,8 @@ export class AdminAbsenceEditModalComponent implements OnInit, OnDestroy {
           );
       }
     } else {
-      this.isTypeEdit.value = true;
-      this.isSummaryEdit.value = true;
-      this.isBeginEdit.value = true;
-      this.isEndEdit.value = true;
-      this.isReporterEdit.value = true;
-      this.isAssigneeEdit.value = true;
-      this.isDurationEdit.value = true;
-      this.message = "Save";
+      this.setFormEdited(true);
     }
-  }
-
-  onEdit(element) {
-    element.value = true;
-    this.message = "Save";
   }
 
   public countDuration(): number {
@@ -180,8 +121,43 @@ export class AdminAbsenceEditModalComponent implements OnInit, OnDestroy {
 
   changeHandler(event): number {
     this.countDuration();
-
     return this.duration;
+  }
+
+  public isFormEdited(): boolean {
+    return (
+      this.formEditState.isTypeEdited ||
+      this.formEditState.isSummaryEdited ||
+      this.formEditState.isBeginEdited ||
+      this.formEditState.isEndEdited ||
+      this.formEditState.isDurationEdited ||
+      this.formEditState.isReporterEdited ||
+      this.formEditState.isAssigneeEdited
+    );
+  }
+
+  public setFormEdited(value: boolean) {
+    this.formEditState.isTypeEdited = value;
+    this.formEditState.isSummaryEdited = value;
+    this.formEditState.isBeginEdited = value;
+    this.formEditState.isEndEdited = value;
+    this.formEditState.isDurationEdited = value;
+    this.formEditState.isReporterEdited = value;
+    this.formEditState.isAssigneeEdited = value;
+  }
+
+  public getFormData() {
+    this.absence.type = this.createAbsenceForm.controls["type"].value;
+    this.absence.summary = this.createAbsenceForm.controls["summary"].value;
+    this.absence.begin = Absence.convertDate(
+      this.createAbsenceForm.controls["begin"].value
+    );
+    this.absence.end = Absence.convertDate(
+      this.createAbsenceForm.controls["end"].value
+    );
+    this.absence.reporter = this.createAbsenceForm.controls["reporter"].value;
+    this.absence.assignee = this.createAbsenceForm.controls["assignee"].value;
+    this.absence.duration = this.createAbsenceForm.controls["duration"].value;
   }
 
   public onCancel(): void {
