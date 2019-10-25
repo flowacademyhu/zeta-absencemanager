701afcfd3176cbafdd8c196521206d976132512d
Richard Csiki <richard.csiki@gmail.com>
2019-10-25 14:02:50 +0200

    dataC deleted

--- src/main/java/hu/flowacademy/zetaabsencemanager/model/PasswordChangeDTO.java
+++ src/main/java/hu/flowacademy/zetaabsencemanager/model/PasswordChangeDTO.java
@@ -9,13 +9,11 @@ public class PasswordChangeDTO {
 
     private String firstPassword;
     private String secondPassword;
-    private String dataC;
 
 
     public PasswordChangeDTO(String firstPassword, String secondPassword, String dataC) {
         this.firstPassword = firstPassword;
         this.secondPassword = secondPassword;
-        this.dataC = dataC;
     }
 
     public PasswordChangeDTO(String firstPassword, String secondPassword) {
